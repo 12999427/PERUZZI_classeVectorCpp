b6255206266d2802ab2cfe3e69509eefa579d986
12999427 <12999427@itisrossi.vi.it>
2026-03-02 13:24:01 +0100

    Initial commit

---
+++ MyVector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 12999427 on 02/03/2026.
+//
+
+#include "MyVector.h"
---
+++ MyVector.h
@@ -0,0 +1,53 @@
+//
+// Created by 12999427 on 02/03/2026.
+//
+
+#ifndef PERUZZI_CLASSEVECTORCPP_MYVECTOR_H
+#define PERUZZI_CLASSEVECTORCPP_MYVECTOR_H
+#include <iosfwd>
+
+
+class MyVectorInt {
+private:
+    int * data;
+    int size;
+    int capacity;
+
+public:
+    MyVectorInt();
+    MyVectorInt(int numElementsToZero);
+    MyVectorInt(const MyVectorInt& other);
+    ~MyVectorInt();
+    int GetSize() const;
+    int GetCapacity() const;
+    bool IsEmpty();
+    int& At(int index);
+    const int& At (int index) const;
+    int& operator[](int index);
+    const int& operator[](int index) const;
+
+    void SetElement(int index, int element);
+    void Push_Back(int value);
+    int Pop_Back();
+    void Clear();
+    void Resize(int newSize);
+    void Reserve(int newCapacity);
+    void operator= (const MyVectorInt & other);
+
+    void Sort();
+    void Sort(bool ascending);
+    void Reverse();
+    int Find(int value) const;
+    bool Contains(int value) const;
+
+    //friend per stampare
+    friend std::ostream& operator<<(std::ostream& os, const MyVectorInt& v);
+
+    //facoltativi
+    void Insert(int pos, int value);
+    void erease (int pos);
+    bool operator==(const MyVectorInt &) const;
+};
+
+
+#endif //PERUZZI_CLASSEVECTORCPP_MYVECTOR_H
